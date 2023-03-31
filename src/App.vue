ea58c37962da661f602ae77c256140c920e5f16f
Yannic-reust <62382435+Yannic-reust@users.noreply.github.com>
2023-03-31 15:16:00 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script></script>
+
+<template>
+  <div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<style src="./app.scss"></style>
---
+++ src/views/Home/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="Home" class="bg-primary">
+    <h1>awsdasd</h1>
+  </div>
+</template>
+<script lang="ts">
+export default {
+  name: "Home",
+  components: {},
+  setup() {},
+
+  data() {
+    return {};
+  },
+
+  methods: {},
+};
+</script>
+
+<style scoped src="./Home.scss"></style>
