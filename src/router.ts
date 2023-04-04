7ced8e199ce70209e513138d543675f7b414bde7
Yannic-reust <62382435+Yannic-reust@users.noreply.github.com>
2023-04-04 08:19:17 +0200

    added Search page

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
 import { createWebHistory, createRouter, RouteRecordRaw } from "vue-router";
 import Home from "./views/Home/Home.vue";
 import DetailSearch from "./views/DetailSearch/DetailSearch.vue";
+import Search from "./views/Search/Search.vue";
 
 
 
@@ -15,6 +16,11 @@ const routes: Array<RouteRecordRaw> =  [
     path: "/detailSearch",
     name: "detailSearch",
     component: DetailSearch,
+  },
+  {
+    path: "/Search",
+    name: "Search",
+    component: Search,
   },
   
 ];
---
+++ src/views/Search/Search.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="Search">
+    <h1>Search</h1>
+    <h2>{{ store.state }}</h2>
+
+    <input type="text" class="input-text" />
+  </div>
+</template>
+<script lang="ts">
+import { useStore } from "vuex";
+export default {
+  name: "Search",
+  data() {
+    return {
+      store: useStore(),
+    };
+  },
+
+  methods: {},
+  created() {
+    console.log(this.store.state.searchText);
+  },
+};
+</script>
+
+<style scoped src="./Search.scss"></style>
