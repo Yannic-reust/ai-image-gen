c94bb48a9b44828865ce9c3a5fafc939d92c5fe6
Yannic-reust <62382435+Yannic-reust@users.noreply.github.com>
2023-04-04 08:18:59 +0200

    added footer

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
-<script></script>
+<script lang="ts">
+//@ts-ignore
+import FooterComponent from "./Components/Footer/FooterComponent.vue";
+export default {
+  components: {
+    FooterComponent,
+  },
+};
+</script>
 
 <template>
   <div>
     <router-view></router-view>
+    <div class="margin-top-5">
+      <FooterComponent></FooterComponent>
+    </div>
   </div>
 </template>
 
